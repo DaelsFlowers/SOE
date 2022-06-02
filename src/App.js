ce514ef2011dc82ee76b03113ebca401128be52c
DaelsFlowers <danieel.flowers@gmail.com>
2022-06-02 03:34:31 -0500

    fixed

--- src/App.js
+++ src/App.js
@@ -2,11 +2,6 @@ import "./bootstrap.min (2).css"
 import './App.css';
 import PreLoader1 from "./Components/preloader";
 
-import Forms from "./Components/Form.js";
-import loadingGif from "./Components/fonts/INTENTO2.gif"
-
-import Navbar from "./Components/Navbar";
-
 function App() {
 
   return (
--- src/Components/preloader.js
+++ src/Components/preloader.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import ReactLoading from "react-loading";
 import animaton from "./fonts/INTENTO2.gif"
 import Forms from "./Form";
 import Navbar from "./Navbar";
